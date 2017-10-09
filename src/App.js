d737fba6a3079d425001a1761d60c6644f74361d
Mariley20 <mariley2018@gmail.com>
2017-10-09 17:28:22 -0500

    componentes linkeados

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
             <h1 className = "App-title" > Welcome to React </h1> </header> <p className = "App-intro" >
             To get started, edit <code> src / App.js </code> and save to reload. </p>
             <div>
-              <button className="btn btn-primary">agregar</button>
+              <button className="btn btn-primary">App pagina</button>
             </div>
             </div>
         );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import App from './ValidarTelefono';
+// import App from './ValidarDatos';
+// import App from './MapaGoogle';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
