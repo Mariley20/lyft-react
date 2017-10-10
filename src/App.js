dee7853f325c409213ea40fe7fb0bd8c19beff4e
Mariley20 <mariley2018@gmail.com>
2017-10-10 09:40:43 -0500

    primera pagina

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@ import './App.css';
 
 class App extends Component {
     render() {
-        return ( <div className = "App" >
-            <header className = "App-header" >
-            <img src = { logo } className = "App-logo" alt = "logo" />
-            <h1 className = "App-title" > Welcome to React </h1> </header> <p className = "App-intro" >
-            To get started, edit <code> src / App.js </code> and save to reload. </p>
-            <div>
-              <button className="btn btn-primary">App pagina</button>
+        return (
+          <div class="fluid-container">
+            <div id="app-content">
+              <header>
+                <div><img src="assets/img/logo.png" class="img-responsive logo" /></div>
+              </header>
+              <section>
+                <div class="container">
+                  <div class="row ">
+                    <div class="col-sm-6 col-xs-6 btn-container text-right">
+				                <a class="btn btn-1 btn-lg" id="login" href="#" role="button">Log in</a>
+                    </div>
+                    <div class="col-sm-6 col-xs-6 btn-container ">
+				                <a class="btn btn-2 btn-lg" href="index-phone.html" role="button">Sign up</a>
+                    </div>
+                  </div>
+                </div>
+              </section>
             </div>
-            </div>
+          </div>
         );
     }
 }
--- src/MapaGoogle.js
+++ src/MapaGoogle.js
@@ -1,20 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './MapaGoogle.css';
+// import { GoogleMap, Marker } from "react-google-maps";
 
-class App extends Component {
-    render() {
-        return ( <div className = "App" >
-            <header className = "App-header" >
-            <img src = { logo } className = "App-logo" alt = "logo" />
-            <h1 className = "App-title" > Welcome to React </h1> </header> <p className = "App-intro" >
-            To get started, edit <code> src / App.js </code> and save to reload. </p>
-            <div>
-              <button className="btn btn-primary">Validar Telefono pagina</button>
-            </div>
-            </div>
-        );
-    }
+import { compose, withProps } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} onClick={props.onMarkerClick} />}
+  </GoogleMap>
+)
+
+class App extends React.Component {
+  state = {
+    isMarkerShown: false,
+  }
+  componentDidMount () {
+    this.delayedShowMarker();
+  }
+  delayedShowMarker = () => {
+    setTimeout(() => {
+      this.setState({ isMarkerShown: true })
+    }, 3000)
+  }
+
+  handleMarkerClick = () => {
+    this.setState({ isMarkerShown: false })
+    this.delayedShowMarker()
+  }
+
+  render() {
+    return (
+      <MyMapComponent
+        isMarkerShown={this.state.isMarkerShown}
+        onMarkerClick={this.handleMarkerClick}
+      />)
+  }
 }
 
 export default App;
